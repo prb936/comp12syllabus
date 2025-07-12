5ed824db827cc974d2a628d21157dcb8f60d2ed9
prb936 <024NEB936@sxc.edu.np>
2025-07-12 13:18:38 +0545

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int ret();
+
+int main()
+{ int a=ret();
+printf("Your lucky number: %d \n",a);
+printf("Executed by Pratik Raj Bista");
+    return 0;
+}
+int ret()
+{
+ return 36;
+}
